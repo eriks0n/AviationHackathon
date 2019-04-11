785f84eaf8d0ed74f1437e867a1e7fe73be1b9ec
Lennart Erikson <lennart.erikson@nordakademie.de>
2019-04-11 11:22:59 +0200

    Initial Commit

---
+++ AviationHackathon/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AviationHackathon
+//
+//  Created by Lennart Erikson on 11.04.19.
+//  Copyright © 2019 Lennart Erikson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
