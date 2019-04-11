511d3e16f2ac3d5e35a5a1758c721477e42893f5
Lennart Erikson <lennart.erikson@nordakademie.de>
2019-04-11 19:38:31 +0200

    Starting implementation of TemperatureViewController

---
+++ AviationHackathon/View Controllers/TemperatureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TemperatureViewController.swift
+//  AviationHackathon
+//
+//  Created by Lennart Erikson on 11.04.19.
+//  Copyright © 2019 Lennart Erikson. All rights reserved.
+//
+
+import UIKit
+
+class TemperatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
